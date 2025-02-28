9d1d86f9cae8ebcd9d78f85b46036ccf3c2e0f02
Krushna <krushnaankade01@gmail.com>
2025-02-28 16:28:44 +0530

    use react-router

---
+++ src/Login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="">
+      Login{" "}
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+        asperiores delectus! Accusantium consequatur dolor et exercitationem
+        suscipit quas vel ea explicabo sapiente. Facilis similique voluptatibus
+        nihil mollitia dolores architecto ipsa?
+      </h1>
+    </div>
+  );
+};
+
+export default Login;
